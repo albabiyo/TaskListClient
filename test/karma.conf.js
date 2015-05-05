4bbbd50d3112e3b58624f9acabb8f2c0bb52165a
alonmyburgh <alonmyburgh@gmail.com>
2015-05-05 17:26:35 +0300

    Added Karma conf from OS, with some edits of mine

---
+++ test/karma.conf.js
@@ -0,0 +1,30 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : './',
+
+    files : [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine',
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
