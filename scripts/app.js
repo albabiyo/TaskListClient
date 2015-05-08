69de0f4ea4fdccd2af3fa6ceb6305358cf6ed83e
NetanelSaka <nsaka@walla.com>
2015-05-08 09:04:58 +0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -4,13 +4,13 @@ angular.module('TaskList', [])
     $scope.tasklist = [];
     
      function refresh() {
-        $http.get('http://52.24.38.84:8080//Tasks').success(function (data, status, headers, config) {
+        $http.get('http://52.24.38.84:8080/Tasks').success(function (data, status, headers, config) {
             $scope.tasklist = data;
         });
     }
     
     $scope.addTask = function(newTask) {
-        $http.post('http://52.24.38.84:8080//Tasks', {des: newTask}).success(function () {
+        $http.post('http://52.24.38.84:8080/Tasks', {des: newTask}).success(function () {
             refresh();
         })
          $scope.taskName = "";
@@ -18,7 +18,7 @@ angular.module('TaskList', [])
     }
 
      $scope.deleteTask = function (task) {
-        $http.delete('http://52.24.38.84:8080//Tasks/' + task.id).success(function () {
+        $http.delete('http://52.24.38.84:8080/Tasks/' + task.id).success(function () {
             refresh();
         });
     };
